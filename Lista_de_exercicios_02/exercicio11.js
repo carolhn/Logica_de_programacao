93a7377a76b561a4f832ae85d3aeefa526f9ef1f
Caroline Nunes <nunescaroline905@gmail.com>
2023-08-24 22:22:19 -0300

    feat: add new exercise 12

---
+++ Lista_de_exercicios_02/exercicio11.js
@@ -0,0 +1,6 @@
+// 11. Faça um algoritmo que receba o mes e ano de nascimento de uma pessoa e o mes
+// e ano atual, calcule e mostre a idade desta pessoa em anos, meses e dias. Como existe a
+// possibilidade do usuario digitar o ano atual menor do que o ano de nascimento, ou os meses
+// foram do intervalo de 1 à 12, deverá ser exibido a mensagem “Impossivel realizar o calculo. Anos
+// e/ou meses invalidos”. Considere para o mes inicial o primeiro dia e para o mes final o ultimo dia
+// do respectivo mês.
